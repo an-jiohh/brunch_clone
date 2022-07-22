acc5d21f30acac005b3c3549e9fa70d34c532168
an-jiohh <dkswlgh99@naver.com>
2022-07-23 01:04:58 +0900

    slider

---
+++ slider.js
@@ -0,0 +1,19 @@
+document.querySelector(".page_img1").addEventListener("click", function () {
+  document.querySelector(".picture_container").style.transform =
+    "translate(0px)";
+});
+
+document.querySelector(".page_img2").addEventListener("click", function () {
+  document.querySelector(".picture_container").style.transform =
+    "translate(-980px)";
+});
+
+document.querySelector(".page_img3").addEventListener("click", function () {
+  document.querySelector(".picture_container").style.transform =
+    "translate(-1960px)";
+});
+
+document.querySelector(".page_img4").addEventListener("click", function () {
+  document.querySelector(".picture_container").style.transform =
+    "translate(-2940px)";
+});
